ba27a8b84e6fd15e1015feb0a177e1bca009b56c
TiagoDominguez-code <tiagodo630@gmail.com>
2025-09-03 12:16:53 -0300

    seccion de tarjetas para productos destacados en desarrollo. archivo .js para la informacion de los productos

---
+++ CardsData.js
@@ -0,0 +1,42 @@
+const productosDestacados = [
+    {
+        imgagen: "G:\Tiago\ITBA\repositorio\ITBA\Imagenes\Aparador Uspallata.png",
+        nombre: "Aparador Uspallata",
+        detalle: "Aparador de seis puertas fabricado en nogal sostenible con tiradores metálicos en acabado latón. Su silueta minimalista realza el veteado natural de la madera, creando una pieza que combina funcionalidad y elegancia atemporal para espacios contemporáneos.",
+        medidas: "180 x 45 x 75 cm",
+        materiales: "Nogal macizo FSC®, herrajes de latón",
+        acabado: "Aceite natural ecológico",
+        peso: "68 Kg",
+        capacidad: "6 compartimentos interiores",
+    },
+    {
+        imgagen: "G:\Tiago\ITBA\repositorio\ITBA\Imagenes\Butaca Mendoza.png",
+        nombre: "Butaca Mendoza",
+        detalle: "Butaca tapizada en bouclé Dusty Rose con base de madera de guatambú. El respaldo curvo abraza el cuerpo y ofrece máximo confort, mientras que su diseño orgánico aporta calidez y sofisticación a cualquier ambiente contemporáneo.",
+        medidas: "80 x 75 x 85 cm",
+        materiales: "Guatambú macizo, tela bouclé",
+        acabado: "Cera vegetal, tapizado premium",
+        Tapizado: "Repelente al agua y manchas",
+        confort: "Espuma Alta densidad",
+    },
+    {
+        imgagen: 
+        nombre:
+        detalle:
+        medidas:
+        materiales:
+        acabado:
+        peso:
+        capacidad:
+    },
+    {
+        imgagen: 
+        nombre:
+        detalle:
+        medidas:
+        materiales:
+        acabado:
+        peso:
+        capacidad:
+    },
+]
