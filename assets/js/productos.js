52a3e9684389c8e935cb764f5344eeb1900c7bc8
fxex <hugoantoniofrey2003@gmail.com>
2025-09-05 20:34:39 -0300

    Refactor: se extrajo los estilos del navegador en un archivo que se puede importar

---
+++ assets/js/productos.js
@@ -0,0 +1,10 @@
+/* 
+"Queremos que los clientes puedan ver todos nuestros productos 
+en un solo lugar, de forma ordenada y atractiva. La página debe ser una
+grilla fácil de navegar visualmente."
+*/
+// Mostrar todos los productos disponibles en una grilla de tarjetas.
+// La información de los productos se cargará desde un archivo JavaScript local.
+//Cada producto debe ser un enlace a su página de detalle.
+// Debe incluir un campo de búsqueda (la funcionalidad es un bonus, pero el elemento debe existir).
+
