5a0e42d1b92256ddda89ba6f45919b705af18861
fxex <hugoantoniofrey2003@gmail.com>
2025-09-07 20:30:30 -0300

    Feat: Simular el envio del contacto

--- README.md
+++ README.md
@@ -13,3 +13,8 @@ Las tecnologias que fueron utilizadas para el desarrollo de este producto son:
 
 ## Funcionalidad implementada
 En el proyecto en esta primera iteración se tendra un sitio web visualmente atractivo, completamente responsivo y con la lógica de interacción necesaria para simular una experiencia de compra.
+Las funcionalidades implementadas son:
+- Carrito de compra: Un carrito en el que se encuentra en toda la aplicacion en el navegador de la aplicacion y se va incrementando para cada producto añadido.
+- Formulario de Contacto: Se genera un formulario para contactarse al cliente. Esto simula el contacto y representa un mensaje en el .DOM
+- Lista de productos: En esta pantalla se cargan los productos que cuenta la muebleria para que sean vendidos.
+- Diseño responsivo: La aplicación funciona correctamente en celulares, tabletas y computadoras.
--- contacto/main.js
+++ contacto/main.js
@@ -1,1 +1,24 @@
+const nombreUsuario = document.getElementById("nombreUsuario")
+const emailUsuario = document.getElementById("emailUsuario")
+const mensajeUsuario = document.getElementById("mensajeUsuario")
+const botonFormulario = document.getElementById("boton_formulario")
+const mensajeError = document.getElementById("error")
 
+botonFormulario.addEventListener("click", (e)=>{
+    e.preventDefault()
+    mensajeError.classList.remove("error")
+    mensajeError.classList.remove("exito")
+    mensajeError.classList.add("error")
+    if (nombreUsuario.value == "") {
+        mensajeError.textContent = "El nombre de usuario no puede estar vacio."
+    }else if (emailUsuario.value == "" ) {
+        mensajeError.textContent = "El email no puede estar vacio."
+    }else if (mensajeUsuario.value == ""){
+        mensajeError.textContent = "El mensaje no puede estar vacio."
+    }else{
+        mensajeError.classList.remove("error")
+        mensajeError.classList.add("exito")
+        mensajeError.textContent = "El mensaje fue enviado."
+    }
+
+})
